6375c8c74ee339b2521326a128fc215cfac3b0e4
Neo <v.saraiva.andrade@gmail.com>
2025-01-22 20:52:20 +0100

    update config custom domain

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/residency-planner',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/residency-planner/',
   trailingSlash: true,
 }
 
